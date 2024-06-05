df5d137124f653959442b3d1891c911c45ae6cbb
supernovahs <supernovahs@proton.me>
2024-06-06 00:58:55 +0530

    get json files for contracts in json_abi folder

--- operator/crates/operator/src/newtask.rs
+++ operator/crates/operator/src/newtask.rs
@@ -1,1 +1,29 @@
+use alloy_sol_types::sol;
 
+sol!(
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    HelloWorldServiceManager,
+    "json_abi/HelloWorldServiceManager.json"
+);
+
+sol!(
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    ECDSAStakeRegistry,
+    "json_abi/ECDSAStakeRegistry.json"
+);
+
+sol!(
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    DelegationManager,
+    "json_abi/DelegationManager.json"
+);
+
+sol!(
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    IAVSDirectory,
+    "json_abi/IAVSDirectory.json"
+);
