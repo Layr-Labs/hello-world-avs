b8df9734118686bb5cc378da13c88de80148d2b9
stompesi <stompesi@gmail.com>
2024-07-22 11:22:47 +0900

    Refactoring Ssal contract

--- contracts/src/Ssal.sol
+++ contracts/src/Ssal.sol
@@ -5,74 +5,77 @@ pragma solidity ^ 0.8.24;
 // import "hardhat/console.sol";
 
 contract Ssal {
-    mapping(bytes32 => Rollup) private rollups;
+    mapping(bytes32 => ProposerSet) private proposerSets;
 
     uint256 public constant MAX_SEQUENCER_COUNT = 30;
+    uint256 public constant BLOCK_MARGIN = 7;
 
-    struct Rollup {
-        address owner;
-        mapping(address => bool) isRegisteredSequencer;
-        address[MAX_SEQUENCER_COUNT] sequencerAddresses;
-        uint256 currentSequencerCount;
-        uint ethBlockMargin;
-    }
-
-    event InitializeRollup(bytes32 rollupId);
-    event RegisterSequencer(bytes32 rollupId, address sequencerAddr);
-    event DeregisterSequencer(bytes32 rollupId, address sequencerAddr);
-
-    function getSequencers(bytes32 rollupId) public view returns(address[] memory) {
-        Rollup storage rollup = rollups[rollupId];
-        address[] memory sequencerList = new address[](rollup.currentSequencerCount);
-        uint256 count = 0;
-        for (uint256 i = 0; i < MAX_SEQUENCER_COUNT; i++) {
-            if (rollup.isRegisteredSequencer[rollup.sequencerAddresses[i]]) {
-                sequencerList[count] = rollup.sequencerAddresses[i];
-                count++;
-            }
-        }
-        return sequencerList;
-    }
-
-    function initializeRollup() public {
-        bytes32 rollupId = keccak256(abi.encodePacked(msg.sender, blockhash(block.number - 1)));
+    struct ProposerSet {
+        address owner; // Owner is rollup contract address
         
-        Rollup storage rollup = rollups[rollupId];
-        rollup.owner = msg.sender;
-        rollup.currentSequencerCount = 0;
+        mapping(address => bool) isRegisteredProposer;
+        address[MAX_SEQUENCER_COUNT] proposerAddresses;
+        uint256 currentProposerCount;        
+    }
+
+    event InitializeProposerSet(bytes32 proposerSetId, address owner);
+    event RegisterProposer(bytes32 proposerSetId, address proposerAddress);
+    event DeregisterProposer(bytes32 proposerSetId, address proposerAddress);
+
+    function initializeProposerSet() public {
+        bytes32 proposerSetId = keccak256(abi.encodePacked(msg.sender, blockhash(block.number - 1)));
         
-        emit InitializeRollup(rollupId);
+        ProposerSet storage proposerSet = proposerSets[proposerSetId];
+        proposerSet.owner = msg.sender;
+        proposerSet.currentProposerCount = 0;
+        
+        emit InitializeProposerSet(proposerSetId, msg.sender);
     }
 
-    function registerSequencer(bytes32 rollupId) public {
-        Rollup storage rollup = rollups[rollupId];
-        require(!rollup.isRegisteredSequencer[msg.sender], "Already registered sequencer");
-        require(rollup.currentSequencerCount < MAX_SEQUENCER_COUNT, "Max sequencer count exceeded");
+    function registerProposer(bytes32 proposerSetId) public {
+        ProposerSet storage proposerSet = proposerSets[proposerSetId];
 
-        rollup.isRegisteredSequencer[msg.sender] = true;
-        rollup.sequencerAddresses[rollup.currentSequencerCount] = msg.sender;
-        rollup.currentSequencerCount++;
+        require(!proposerSet.isRegisteredProposer[msg.sender], "Already registered proposer");
+        require(proposerSet.currentProposerCount < MAX_SEQUENCER_COUNT, "Max proposer count exceeded");
 
-        emit RegisterSequencer(rollupId, msg.sender);
-    }
+        proposerSet.isRegisteredProposer[msg.sender] = true;
 
-    function deregisterSequencer(bytes32 rollupId) public {
-        Rollup storage rollup = rollups[rollupId];
-        require(rollup.isRegisteredSequencer[msg.sender], "Not registered sequencer");
-
-        rollup.isRegisteredSequencer[msg.sender] = false;
-
-        // Remove sequencer from the array
-        for (uint256 i = 0; i < rollup.currentSequencerCount; i++) {
-            if (rollup.sequencerAddresses[i] == msg.sender) {
-                rollup.sequencerAddresses[i] = rollup.sequencerAddresses[rollup.currentSequencerCount - 1];
-                rollup.sequencerAddresses[rollup.currentSequencerCount - 1] = address(0);
+        // Remove proposer from the array
+        for (uint256 i = 0; i < proposerSet.currentProposerCount; i++) {
+            if (proposerSet.proposerAddresses[i] == address(0)) {
+                proposerSet.proposerAddresses[i] = msg.sender;
                 break;
             }
         }
 
-        rollup.currentSequencerCount--;
+        proposerSet.currentProposerCount++;
 
-        emit DeregisterSequencer(rollupId, msg.sender);
+        emit RegisterProposer(proposerSetId, msg.sender);
+    }
+
+    function deregisterProposer(bytes32 proposerSetId) public {
+        ProposerSet storage proposerSet = proposerSets[proposerSetId];
+
+        require(proposerSet.isRegisteredProposer[msg.sender], "Not registered proposer");
+
+        proposerSet.isRegisteredProposer[msg.sender] = false;
+
+        // Remove proposer from the array
+        for (uint256 i = 0; i < proposerSet.currentProposerCount; i++) {
+            if (proposerSet.proposerAddresses[i] == msg.sender) {
+                proposerSet.proposerAddresses[i] = address(0);
+                break;
+            }
+        }
+
+        proposerSet.currentProposerCount--;
+
+        emit DeregisterProposer(proposerSetId, msg.sender);
+    }
+
+    function getProposerList(bytes32 proposerSetId) public view returns(address[] memory) {
+        ProposerSet storage proposerSet = proposerSets[proposerSetId];
+      
+        return proposerSet.proposerAddresses;
     }
 }
