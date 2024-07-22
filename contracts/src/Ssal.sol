0fc322ee513eda1e2431c14dbee176b8bcf6b544
stompesi <stompesi@gmail.com>
2024-07-22 11:03:37 +0900

    Refactoring Ssal contract

--- contracts/src/Ssal.sol
+++ contracts/src/Ssal.sol
@@ -1,57 +1,78 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
+pragma solidity ^ 0.8.24;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
 contract Ssal {
-    mapping(bytes32 => Cluster) clusters;
-    struct Cluster {
+    mapping(bytes32 => Rollup) private rollups;
+
+    uint256 public constant MAX_SEQUENCER_COUNT = 30;
+
+    struct Rollup {
         address owner;
-        address rollup;
+        mapping(address => bool) isRegisteredSequencer;
+        address[MAX_SEQUENCER_COUNT] sequencerAddresses;
+        uint256 currentSequencerCount;
         uint ethBlockMargin;
-        address[30] sequencers;
     }
 
-    event InitializeClusterEvent(
-        bytes32 clusterID
-    );
+    event InitializeRollup(bytes32 rollupId);
+    event RegisterSequencer(bytes32 rollupId, address sequencerAddr);
+    event DeregisterSequencer(bytes32 rollupId, address sequencerAddr);
 
-    event registerSequencerEvent(
-        string ip
-    );
-
-    function getSequencers(bytes32 clusterID) public view returns (address[30] memory){        
-        return clusters[clusterID].sequencers;
+    function getSequencers(bytes32 rollupId) public view returns(address[] memory) {
+        Rollup storage rollup = rollups[rollupId];
+        address[] memory sequencerList = new address[](rollup.currentSequencerCount);
+        uint256 count = 0;
+        for (uint256 i = 0; i < MAX_SEQUENCER_COUNT; i++) {
+            if (rollup.isRegisteredSequencer[rollup.sequencerAddresses[i]]) {
+                sequencerList[count] = rollup.sequencerAddresses[i];
+                count++;
+            }
+        }
+        return sequencerList;
     }
 
-    function initializeCluster() public {
-        bytes32 clusterID = keccak256(abi.encodePacked(msg.sender, blockhash(block.number-1)));
-        clusters[clusterID].owner = msg.sender;
-        clusters[clusterID].rollup = msg.sender;
+    function initializeRollup() public {
+        bytes32 rollupId = keccak256(abi.encodePacked(msg.sender, blockhash(block.number - 1)));
         
-        emit InitializeClusterEvent(clusterID);
+        Rollup storage rollup = rollups[rollupId];
+        rollup.owner = msg.sender;
+        rollup.currentSequencerCount = 0;
+        
+        emit InitializeRollup(rollupId);
     }
 
-    function registerSequencer(bytes32 clusterID, address _sequencer, string memory ip) public {
-        uint index = 30;
-        for (uint i = 0; i < 30; i++) {
-            if(clusters[clusterID].sequencers[i] == _sequencer) revert();
-            if(clusters[clusterID].sequencers[i] == address(0)) {
-                index = i;
+    function registerSequencer(bytes32 rollupId) public {
+        Rollup storage rollup = rollups[rollupId];
+        require(!rollup.isRegisteredSequencer[msg.sender], "Already registered sequencer");
+        require(rollup.currentSequencerCount < MAX_SEQUENCER_COUNT, "Max sequencer count exceeded");
+
+        rollup.isRegisteredSequencer[msg.sender] = true;
+        rollup.sequencerAddresses[rollup.currentSequencerCount] = msg.sender;
+        rollup.currentSequencerCount++;
+
+        emit RegisterSequencer(rollupId, msg.sender);
+    }
+
+    function deregisterSequencer(bytes32 rollupId) public {
+        Rollup storage rollup = rollups[rollupId];
+        require(rollup.isRegisteredSequencer[msg.sender], "Not registered sequencer");
+
+        rollup.isRegisteredSequencer[msg.sender] = false;
+
+        // Remove sequencer from the array
+        for (uint256 i = 0; i < rollup.currentSequencerCount; i++) {
+            if (rollup.sequencerAddresses[i] == msg.sender) {
+                rollup.sequencerAddresses[i] = rollup.sequencerAddresses[rollup.currentSequencerCount - 1];
+                rollup.sequencerAddresses[rollup.currentSequencerCount - 1] = address(0);
                 break;
             }
         }
-        if (index == 30) revert();
-        else clusters[clusterID].sequencers[index] = _sequencer;
 
-        emit registerSequencerEvent(ip);
-    }
+        rollup.currentSequencerCount--;
 
-    function deregisterSequencer(bytes32 clusterID, address _sequencer) public {
-        for (uint i = 0; i < 30; i++) {
-            if(clusters[clusterID].sequencers[i] == _sequencer)
-                clusters[clusterID].sequencers[i] = address(0);
-        }
+        emit DeregisterSequencer(rollupId, msg.sender);
     }
 }
