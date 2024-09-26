50707a172188b2e985ea10c8d5a29da12bba5a84
kachapah <60323455+Sidu28@users.noreply.github.com>
2024-09-26 08:04:06 -0700

    separate import for SM

--- contracts/script/HelloWorldDeployer.s.sol
+++ contracts/script/HelloWorldDeployer.s.sol
@@ -4,13 +4,14 @@ pragma solidity ^0.8.0;
 import {Script} from "forge-std/Script.sol";
 import {console2} from "forge-std/Test.sol";
 import {HelloWorldDeploymentLib} from "./utils/HelloWorldDeploymentLib.sol";
-import {CoreDeploymentLib, StrategyManager} from "./utils/CoreDeploymentLib.sol";
+import {CoreDeploymentLib} from "./utils/CoreDeploymentLib.sol";
 import {UpgradeableProxyLib} from "./utils/UpgradeableProxyLib.sol";
 import {StrategyBase} from "@eigenlayer/contracts/strategies/StrategyBase.sol";
 import {ERC20Mock} from "../test/ERC20Mock.sol";
 import {TransparentUpgradeableProxy} from
     "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {StrategyFactory} from "@eigenlayer/contracts/strategies/StrategyFactory.sol";
+import {StrategyManager} from "@eigenlayer/contracts/core/StrategyManager.sol";
 
 
 import {
