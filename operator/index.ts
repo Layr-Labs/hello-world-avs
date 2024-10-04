3c26b44ffc19e135525ccb6ffc3129ddb758f543
steven <steven.nevins.js@gmail.com>
2024-10-04 11:25:13 -0400

    fix: reimplement signAndRespondToTask

--- operator/index.ts
+++ operator/index.ts
@@ -1,8 +1,10 @@
 import { registerOperator } from "./registerAsOperator";
+import { registerToAvs } from "./registerToAvs";
 import { monitorNewTasks } from "./monitorTasks";
 
 const main = async () => {
     await registerOperator();
+    await registerToAvs();
     monitorNewTasks().catch((error) => {
         console.error("Error monitoring tasks:", error);
     });
--- operator/signAndRespondToTask.ts
+++ operator/signAndRespondToTask.ts
@@ -1,9 +1,35 @@
-
+import { keccak256, toBytes } from 'viem'
+import { writeHelloWorldServiceManager } from "../dist/generated";
+import {config, account, helloWorldServiceManagerAddress } from "./config"
 
 export const signAndRespondToTask = async (taskIndex: number, taskCreatedBlock: number, taskName: string) => {
     console.log(
         `Signing and responding to task ${taskIndex}`
     )
 
-    console.log(`Responded to task.`);
+    try {
+        // Create the task object
+        const task = {
+            name: taskName,
+            taskCreatedBlock: taskCreatedBlock
+        };
+
+        // Create the message to be signed
+        const message = `Hello, ${taskName}`;
+        const messageHash = keccak256(toBytes(message));
+
+        // Sign the message
+        const signature = await account.sign({hash:messageHash});
+
+        // Call the respondToTask function on the HelloWorldServiceManager contract
+        const result = await writeHelloWorldServiceManager(config, {
+            account: account,
+            address: helloWorldServiceManagerAddress,
+            functionName: "respondToTask",
+            args: [task, taskIndex, signature]
+        });
+        // console.log(`Transaction successful with hash: ${result}`);
+    } catch (error) {
+        console.error('Error responding to task:', error);
+    }
 };
