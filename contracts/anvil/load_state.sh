4d9cc0eb89cdae45dcdd8e9620302612660a14be
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-02-20 10:59:50 -0300

    feat: script to deploy contracts to anvil

---
+++ contracts/anvil/load_state.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+STATE_FILE="contracts/anvil/state.json"
+
+mkdir -p "$(dirname "$STATE_FILE")"
+
+echo "Starting Anvil with state dump in background"
+anvil --dump-state "$STATE_FILE" --host 0.0.0.0 --port 8545 --base-fee 0 --gas-price 0 > /dev/null 2>&1 &
+ANVIL_PID=$!
+
+echo "Deploying EigenLayer contracts."
+make deploy-eigenlayer-contracts > /dev/null 2>&1
+
+echo "Deploying HelloWorld contracts."
+make deploy-helloworld-contracts > /dev/null 2>&1
+
+echo "Killed Anvil..."
+kill $ANVIL_PID
