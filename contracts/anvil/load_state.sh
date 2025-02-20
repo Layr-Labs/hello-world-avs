8a95acda79aca44ba7ee1b71ac345ef6d3a92831
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-02-20 11:09:37 -0300

    wip: remove /dev/null to check the output

--- contracts/anvil/load_state.sh
+++ contracts/anvil/load_state.sh
@@ -6,14 +6,14 @@ STATE_FILE="contracts/anvil/state.json"
 mkdir -p "$(dirname "$STATE_FILE")"
 
 echo "Starting Anvil with state dump in background"
-anvil --dump-state "$STATE_FILE" --host 0.0.0.0 --port 8545 --base-fee 0 --gas-price 0 > /dev/null 2>&1 &
+anvil --dump-state "$STATE_FILE" --host 0.0.0.0 --port 8545 --base-fee 0 --gas-price 0 &
 ANVIL_PID=$!
 
 echo "Deploying EigenLayer contracts."
-make deploy-eigenlayer-contracts > /dev/null 2>&1
+make deploy-eigenlayer-contracts 
 
 echo "Deploying HelloWorld contracts."
-make deploy-helloworld-contracts > /dev/null 2>&1
+make deploy-helloworld-contracts
 
 echo "Killed Anvil..."
 kill $ANVIL_PID
