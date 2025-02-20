53dfe7d5f2759bb80908f2aa3867c733846c1b8c
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-02-20 11:56:02 -0300

    Revert "wip: remove /dev/null to check the output"
    This reverts commit 8a95acda79aca44ba7ee1b71ac345ef6d3a92831.

--- contracts/anvil/load-state.sh
+++ contracts/anvil/load-state.sh
@@ -6,7 +6,7 @@ STATE_FILE="contracts/anvil/state.json"
 mkdir -p "$(dirname "$STATE_FILE")"
 
 echo "Starting Anvil with state dump in background"
-anvil --dump-state "$STATE_FILE" &
+anvil --dump-state "$STATE_FILE" --host 0.0.0.0 --port 8545 --base-fee 0 --gas-price 0 > /dev/null 2>&1 &
 ANVIL_PID=$!
 
 sleep 3
@@ -18,10 +18,10 @@ echo "Building contracts"
 make build-contracts
 
 echo "Deploying EigenLayer contracts."
-make deploy-eigenlayer-contracts 
+make deploy-eigenlayer-contracts > /dev/null 2>&1
 
 echo "Deploying HelloWorld contracts."
-make deploy-helloworld-contracts
+make deploy-helloworld-contracts > /dev/null 2>&1
 
 echo "Killed Anvil"
 kill $ANVIL_PID
